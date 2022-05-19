31216125dd59dfbba041b2026abc90736616836a
Ricardo Galan <Ricardo.Galan@ey.com>
2022-05-19 18:14:02 -0300

    added material modules and angular material to the project

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ],
+  exports:[
+    MatTableModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule
+  ]
+})
+export class MaterialModule { }
